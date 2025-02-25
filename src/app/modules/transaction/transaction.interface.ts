0953ebece4c16d5b1ab9d69a0623ec70b6ddc795
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-02-25 11:15:01 +0600

    feat: add Transaction interface for Transaction schema

---
+++ src/app/modules/transaction/transaction.interface.ts
@@ -0,0 +1,16 @@
+import { Types, Document } from "mongoose";
+
+export interface ITransaction extends Document {
+    sender: Types.ObjectId;
+    receiver: Types.ObjectId;
+    amount: number;
+    fees: ITransactionFees;
+    note: string;
+    type: 'send_money' | 'cash_in' | 'cash_out' | 'agent_recharge' | 'agent_withdraw';
+}
+
+export interface ITransactionFees {
+    transactionFee: number;
+    adminFee: number;
+    agentCommission: number;
+}
