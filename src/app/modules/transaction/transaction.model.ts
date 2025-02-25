2e586e8e3571d40345714b912ff88a0e55cc284a
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-02-25 11:15:21 +0600

    feat: add Transaction model and schema

---
+++ src/app/modules/transaction/transaction.model.ts
@@ -0,0 +1,49 @@
+import { model, Schema } from "mongoose";
+import { ITransaction } from "./transaction.interface";
+
+
+const transactionFeesSchema = new Schema({
+    transactionFee: { type: Number, default: 0 },
+    adminFee: { type: Number, default: 0 },
+    agentCommission: { type: Number, default: 0 }
+}, {
+    _id: false
+});
+
+const transactionSchema = new Schema<ITransaction>({
+    sender: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    receiver: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['send_money', 'cash_in', 'cash_out', 'agent_recharge', 'agent_withdraw'],
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true,
+        min: 50
+    },
+    note: {
+        type: String,
+        required: true
+    },
+    fees: {
+        type: transactionFeesSchema,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+
+const Transaction = model<ITransaction>('Transaction', transactionSchema);
+
+export default Transaction;
