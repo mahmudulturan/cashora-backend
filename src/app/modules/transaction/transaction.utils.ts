f573ea3f5d39dfe753d460229dc6f8d81a7281ef
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-02-25 15:45:55 +0600

    feat: add transaction fees calculation utility

---
+++ src/app/modules/transaction/transaction.utils.ts
@@ -0,0 +1,26 @@
+interface TransactionFees {
+    transactionFee: number;
+    adminFee: number;
+    agentCommission: number;
+}
+
+export const calculateTransactionFees = (amount: number, type: 'send_money' | 'cash_out' | 'cash_in'): TransactionFees => {
+    const fees: TransactionFees = {
+        transactionFee: 0,
+        adminFee: 5,
+        agentCommission: 0
+    };
+
+    if (type === 'send_money') {
+        fees.transactionFee = amount > 100 ? 5 : 0;
+    } else if (type === 'cash_out') {
+        fees.transactionFee = amount * 0.015;
+        fees.agentCommission = amount * 0.01;
+        fees.adminFee += amount * 0.005;
+    } else if (type === 'cash_in') {
+        fees.transactionFee = 0;
+    } 
+
+    return fees;
+};
+
