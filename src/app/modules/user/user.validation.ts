26bd658ff1bbe6f6debffac62ddcb0536a77b0f6
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-02-25 14:09:01 +0600

    refactor: simplify user update validation schema

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -5,29 +5,20 @@ const updateUserValidationSchema = z.object({
         name: z.object({
             firstName: z.string({
                 invalid_type_error: 'First name must be a string'
-            }).optional(),
+            }),
             lastName: z.string({
                 invalid_type_error: 'Last name must be a string'
-            }).optional()
+            })
         }, {
             invalid_type_error: 'Name must be an object'
-        }).optional(),
-        dateOfBirth: z.string({
-            invalid_type_error: 'Date of birth must be a date'
-        }).date().optional(),
-        gender: z.enum(['male', 'female', 'other'], {
-            invalid_type_error: 'Gender must be "male", "female" or "other"',
-        }).optional(),
-        profileImg: z.string({
-            invalid_type_error: 'Profile image must be a string'
-        }).optional()
+        })
     })
 })
 
 const updateUserStatusValidationSchema = z.object({
     body: z.object({
         status: z.enum(['active', 'blocked'], {
-            invalid_type_error: 'Status must be "active" or "inactive"',
+            invalid_type_error: 'Status must be "active" or "blocked"',
             required_error: 'Status is required'
         })
     })
